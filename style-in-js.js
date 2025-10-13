4ad93d76687f712c82fec0c2d3a062b0f0eef61b
mosfiqurRahmanJsd <nisho.mushfiq2020@gmail.com>
2025-10-13 19:19:43 +0600

    style in js

---
+++ style-in-js.js
@@ -0,0 +1,15 @@
+const sections = document.querySelectorAll('section'); 
+
+for(const section of sections) {
+    section.style.border = '2px solid steelblue'; 
+    section.style.marginBottom = '20px'; 
+    section.style.borderRadius = '5px'; 
+    section.style.backgroundColor = 'lightgrey'; 
+}
+
+
+
+// const placesContainer = document.getElementById('places-container'); 
+// placesContainer.style.backgroundColor = 'yellow'; 
+
+const styleSection = document.getElementsByClassName('fruits'); 
